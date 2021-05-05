8dadceb567a8bf96f76d312dc8d2425f0bcdc897
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-05-04 19:35:58 -0700

    2.2 task
    Added text to index.html
    
    cleaned up code on server.js

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-//let addr = request.url;
-//let q = url.parse(addr, true);
+const http = require('http'),   
+ fs = require('fs'),
+ url = require('url');
 
-const server = http.createServer(function (request, response) {
-    
-    //const urlObject = url.parse(req.url, true);
-    const addr = request.url;
-    const q = url.parse(addr, true);
-    
-    //const fileName = "." + urlObject.pathname;
-    
-    fs.readFile(addr, function(err, data) {
+http.createServer((request, response) => {
+    let addr = request.url,
+      q = url.parse(addr,true),
+      filePath = '';
 
-        if (err) {
-            response.writeHead(404, {'Content-Type': 'text/html'});
-            return response.end('404 Not Found');
-        }
+if (q.pathname.includes('documentation')) {
+  filePath = (__dirname + './documentation.html');
+} else {
+    filePath = 'index.html';
+}   
 
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(data);
-        response.end('Hello Node!');
-    });
-        
-});
+fs.readFile(filePath, (err, data) => {
+    if (err) {
+        throw err;
+    }
 
-    server.listen(5000, () => {
-    console.log('My first Node test server is running on Port 5000.');
-});
+    response.writeHead(200, { 'Content-Type': 'text/html'});
+    response.write(data);
+    response.end()
+    })
+
+}).listen(5000);
+console.log("My first Node test server is running on Port 5000.");
 
