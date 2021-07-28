d6560c5888a2df74319799245b08230afd649bcb
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-07-27 18:54:29 -0700

    3.6
    back end adjustments to work with 3.6

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const { check, validationResult } = require('express-validator');
 
 const Movies = Models.Movie;
 const Users = Models.User;
-const Directors = Models.Director;
-const Genres = Models.Genre;
+//const Directors = Models.Director;
+let Genres = Models.Genre;
 
 /*mongoose.connect('mongodb://localhost:27017/[myFlixDB]', {
     useNewUrlParser: true,
@@ -97,40 +97,40 @@ app.get('/movies/:Title',
             });
     });
 //Gets list of movies matching a specified genre
-app.get('/genres',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        Genres.find()
-            .then((genres) => {
-                res.json(genres);
-            })
-            .catch((err) => {
-                console.error(err);
-                res.status(500).send('Error ' + err);
-            });
-    });
+app.get('/genres/:name', function
+    //passport.authenticate('jwt', { session: false }),
+    (req, res) {
+    Genres.findOne({ name: req.params.name })
+        .then((genre) => {
+            res.json(genre);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error ' + err);
+        });
+});
 //Gets list of directors
-app.get('/directors',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        Directors.find()
-            .then((directors) => {
-                res.json(directors);
-            })
-            .catch((err) => {
-                console.error(err);
-                res.status(500).send('Error ' + err);
-            });
-    });
+/*app.get('/movies/directors', function
+    //passport.authenticate('jwt', { session: false }),
+    (req, res) {
+    Movies.find()
+        .then((director) => {
+            res.json(director);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error ' + err);
+        });
+});*/
 
 //Gets information for a specified director
 app.get(
-    "/directors/:name",
+    '/movies/directors/:Name',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
-        Directors.findOne({ name: req.params.name })
-            .then((directors) => {
-                res.status(201).json(directors);
+        Movies.findOne({ 'Director.Name': req.params.Name })
+            .then((director) => {
+                res.status(201).json(director);
             })
             .catch((err) => {
                 console.error(err);
@@ -156,8 +156,8 @@ app.post('/users', [
         //passport.authenticate('jwt', { session: false }), 
         let hashedPassword = Users.hashPassword(req.body.Password);
         Users.findOne({ Username: req.body.Username })
-            .then((user) => {
-                if (user) {
+            .then((users) => {
+                if (users) {
                     return res.status(400).send(req.body.Username + 'already exists');
                 } else {
                     Users.create({
@@ -165,7 +165,7 @@ app.post('/users', [
                         Password: hashedPassword,
                         Email: req.body.Email,
                         Birthday: req.body.Birthday
-                    }).then((user) => { res.status(201).json(user) })
+                    }).then((users) => { res.status(201).json(users) })
                         .catch((error) => {
                             console.error(error);
                             res.status(500).send('Error: ' + error);
@@ -192,8 +192,8 @@ app.get('/users', (req, res) => {
 //Get user by username
 app.get('/users/:Username', (req, res) => {
     Users.findOne({ Username: req.params.Username })
-        .then((user) => {
-            res.json(user);
+        .then((users) => {
+            res.json(users);
         })
         .catch((err) => {
             console.error(err);
@@ -264,7 +264,7 @@ app.delete('/users/:Username/Movies/:MovieID',
             $pull: { FavoriteMovies: req.params.MovieID }
         },
             { new: true }, //This line makes usre that the updated document is returned
-            (err, updateUser) => {
+            (err, updatedUser) => {
                 if (err) {
                     console.error(err);
                     res.status(500).send('Error ' + err);
@@ -279,8 +279,8 @@ app.delete('/users/:Username',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
         Users.findOneAndRemove({ Username: req.params.Username })
-            .then((user) => {
-                if (!user) {
+            .then((users) => {
+                if (!users) {
                     res.status(400).send(req.params.Username + ' was not found');
                 } else {
                     res.status(200).send(req.params.Username + ' was deleted');
--- models.js
+++ models.js
@@ -2,38 +2,47 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
-    Title: {type: String, require: true},
-    Description: {type: String, require: true},
-    Director: [{type:mongoose.Schema.Types.ObjectId, ref:"director"}],
-    Genre: [{type:mongoose.Schema.Types.ObjectId, ref: "genre"}],
+    Title: { type: String, require: true },
+    Description: { type: String, require: true },
+    //Director: [{ type: mongoose.Schema.Types.ObjectId, ref: "director" }],
+    //Genre: [{ type: mongoose.Schema.Types.ObjectId, ref: "genre" }],
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String,
+        Birth: Number,
+    },
     Actors: [Array],
     ImagePath: String,
     Featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, require: true},
-    Password: {type: String, require: true},
-    Email: {type: String, require: true},
+    Username: { type: String, require: true },
+    Password: { type: String, require: true },
+    Email: { type: String, require: true },
     Birthday: Date,
-    FavoriteMovie: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
 userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.Password);
 };
 
-let directorSchema = mongoose.Schema({
+/*let directorSchema = mongoose.Schema({
     Director: {
         Name: String,
         Bio: String,
         Birthday: Date
     }
-});
+});*/
 
 let genreSchema = mongoose.Schema({
     Genre: {
@@ -44,10 +53,10 @@ let genreSchema = mongoose.Schema({
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
-let Director = mongoose.model('Director', directorSchema);
+//let Director = mongoose.model('Director', directorSchema);
 let Genre = mongoose.model('Genre', genreSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
-module.exports.Director = Director;
+//module.exports.Director = Director;
 module.exports.Genre = Genre;
